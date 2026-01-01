2db2e0fea5269210222ee2e6a559896aa2af7583
kj15121 <kartikjaiswal.15121@gmail.com>
2026-01-01 14:46:39 +0530

    touch-compatibility

--- LandingPage/core_code.js
+++ LandingPage/core_code.js
@@ -1,8 +1,15 @@
 // The code directly linked to the page
 "use strict"
 
-// Main algorithm objects
-const frame = document.getElementById("main");
+//Main algorithm objects
+const frame = document.getElementById('main');
+
+let camX = 0,
+    camY = 0;
+
+let touchState = false;
+let initX, initY;
+let initCamX, initCamY;
 
 //Logic
 
@@ -14,24 +21,76 @@ frame.animate(
     }
 )
 
-window.onmousemove = function(e) {
-    const cursorX = e.clientX;
-    const cursorY = e.clientY;
-
-    const decX = cursorX / window.innerWidth;
-    const decY = cursorY / window.innerHeight;
-
-    const maxX = frame.offsetWidth - window.innerWidth;
-    const maxY = frame.offsetHeight - window.innerHeight;
-
-    const targetX = maxX * decX * -1;
-    const targetY = maxY * decY * -1;
-
+function render(){
     frame.animate(
-        {transform: `translate(${targetX}px, ${targetY}px)`},
+        {transform: `translate(${camX}px, ${camY}px)`},
         {
             duration: 3000,
             fill: "forwards",
-            easing: "ease",
         })
 }
+
+function viewLimit(){
+    const winX = window.innerWidth,
+        winY = window.innerHeight;
+    const minX = (frame.offsetWidth - winX) * -1,
+        minY = (frame.offsetHeight - winY) * -1;
+    camX = Math.min(0, Math.max(minX, camX));
+    camY = Math.min(0, Math.max(minY, camY));
+}
+
+window.addEventListener(
+    "mousemove",
+    function(e){
+        const winX = window.innerWidth,
+            winY = window.innerHeight;
+        const decX = e.clientX / winX,
+            decY = e.clientY / winY;
+        const maxX = frame.offsetWidth - winX,
+            maxY = frame.offsetHeight - winY;
+
+        camX = decX * maxX * -1;
+        camY = decY * maxY * -1;
+
+        render()
+    }
+);
+
+window.addEventListener(
+    "touchstart",
+    function(e){
+        if (e.touches.length !== 1) return;
+
+        touchState = true;
+        initX = e.touches[0].clientX;
+        initY = e.touches[0].clientY;
+        initCamX = camX;
+        initCamY = camY;
+    },
+    {passive: false}
+);
+
+window.addEventListener(
+    "touchmove",
+    function(e){
+        if (!touchState) return;
+
+        e.preventDefault();
+
+        const moveX = e.touches[0].clientX - initX,
+            moveY = e.touches[0].clientY - initY;
+        camX = initCamX + moveX;
+        camY = initCamY + moveY;
+
+        viewLimit();
+        render();
+    },
+    {passive: false}
+);
+
+window.addEventListener(
+    "touchend",
+    function() {
+        touchState = false;
+    }
+);
